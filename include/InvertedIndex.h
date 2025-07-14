cd9d57c35c155cabcab2346e0ab6b65ceda00de3
Popich (Arsenyi) <arsenyi1103@gmail.com>
2025-07-14 17:46:09 +0300

    Update InvertedIndex.h

--- include/InvertedIndex.h
+++ include/InvertedIndex.h
@@ -1,1 +1,23 @@
+#ifndef INVERTEDINDEX_H
+#define INVERTEDINDEX_H
 
+#include <vector>
+#include <string>
+#include <map>
+
+struct Entry {
+    size_t doc_id;
+    size_t count;
+};
+
+class InvertedIndex {
+private:
+    std::vector<std::string> docs;
+    std::map<std::string, std::vector<Entry>> freq_dictionary;
+
+public:
+    void UpdateDocumentBase(std::vector<std::string> input_docs);
+    std::vector<Entry> GetWordCount(const std::string& word);
+};
+
+#endif // INVERTEDINDEX_H
